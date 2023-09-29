f44a3e946300fbed8d091000a6d537c7e364eeb6
suvendumitraredhat <79252270+suvendumitraredhat@users.noreply.github.com>
2023-09-29 16:06:58 +0300

    Update list_object_noobaa.sh

--- demo/s3/list_object_noobaa.sh
+++ demo/s3/list_object_noobaa.sh
@@ -12,6 +12,5 @@ access_key=$(oc get secrets $bktname -o go-template='{{.data.AWS_ACCESS_KEY_ID |
 # Get secret_key
 secret_key=$(oc get secrets $bktname -o go-template='{{.data.AWS_SECRET_ACCESS_KEY | base64decode }}')
 
-#AWS_ACCESS_KEY_ID=kgE2c5igOjXIQ9ehWoia AWS_SECRET_ACCESS_KEY=pOvNi3qqkM0nRT/L9j/Uy8UT4KKEjKMk6DvmrTC1 aws --endpoint https://a666f11eb677845eca42f1499c0211e9-335652715.eu-west-1.elb.amazonaws.com:443 --no-verify-ssl s3 ls 
-
+echo "AWS_ACCESS_KEY_ID=$access_key AWS_SECRET_ACCESS_KEY=$secret_key aws --endpoint https://$endpoint --no-verify-ssl --recursive s3  ls s3://$bucket_name"
 AWS_ACCESS_KEY_ID=$access_key AWS_SECRET_ACCESS_KEY=$secret_key aws --endpoint https://$endpoint --no-verify-ssl --recursive s3  ls s3://$bucket_name
